700dbcb8788d26c1343a3a364f47afbba27fc457
LDH <ldh1923@naver.com>
2022-06-10 10:49:20 +0900

    3번 문제

--- 220610_Test/220610_Test/220610_Test.cpp
+++ 220610_Test/220610_Test/220610_Test.cpp
@@ -1,11 +1,26 @@
 ﻿#include <iostream>
 
+#define count 5
+
 int main(void)
 {
-    int a = 3;
-    while (a < 1000)
-    {
-        std::cout << a << std::endl;
-        a *= 3;
-    }
+	int a = count - 1;
+	int b = 1;
+
+	for (int i = 0; i < count; i++)
+	{
+		for (int j = a; j > 0; j--)
+		{
+			std::cout << " ";
+		}
+		a -= 1;
+
+		for (int k = 0; k < b; k++)
+		{
+			std::cout << "*";
+		}
+		b += 2;
+
+		std::cout << std::endl;
+	}
 }
