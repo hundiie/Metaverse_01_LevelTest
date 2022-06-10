1fd1b76819fe27e42ccbc16dcfd280f06e5c1f8d
LDH <ldh1923@naver.com>
2022-06-10 15:45:10 +0900

    7번 문제

--- 220610_Test/220610_Test/220610_Test.cpp
+++ 220610_Test/220610_Test/220610_Test.cpp
@@ -3,42 +3,106 @@
 
 int main(void)
 {
-	std::string card[53];
-	std::string mark[4] = { "♠","♥","◆","♣" };
-	std::string num[13] = { "A ","2 ","3 ","4 ","5 ","6 ","7 ","8 ","9 ","10","J ","Q ","K " };
-
-	card[52] = "JOKER";
-
-	for (int k = 0; k < 53; k++)
-	{
-		card[k] = mark[k % 4] + num[k % 13];
-	}
-
+	bool num[25] = { false };
+	int bingo[5][5];
+	bool check[12] = { false };
 	srand(time(NULL));
-	int player[14] = {};
 
-	for (int i = 0; i < 14; i++)
+	for (int i = 0; i < 5; i++)
 	{
-		player[i] = rand() % 53;
-		for (int j = 0; j < i; j++)
+		for (int j = 0; j < 5; j++)
 		{
-			if (player[j] == player[i])
+			int random = rand() % 25;
+			if (num[random] == false)
 			{
-				i--;
-				break;
+				num[random] = true;
+				bingo[i][j] = random + 1;
+			}
+			else
+			{
+				j--;
 			}
 		}
 	}
+	int clear = 0;
 
-	std::cout << "Player1 : ";
-	for (int c = 0; c < 7; c++)
+	while (true)
 	{
-		std::cout << card[player[c]] << " ";
+		//빙고판
+		
+		for (int i = 0; i < 5; i++)
+		{
+			for (int j = 0; j < 5; j++)
+			{
+				if (bingo[i][j] != 0)
+				{
+					std::cout << bingo[i][j] << "\t";
+				}
+				else
+				{
+					std::cout << "\t";
+				}
+			}
+			std::cout << std::endl << std::endl;
+		}
+		
+		//입력
+		int input;
+		std::cout << "현재 " << clear << "줄의 빙고가 완성되었습니다." << std::endl;
+		std::cout << "숫자를 입력해 주세요 : ";
+		std::cin >> input;
+		
+		system("cls");
+		
+		
+		//숫자 지우기
+		for (int i = 0; i < 5; i++)
+		{
+			for (int j = 0; j < 5; j++)
+			{
+				if (bingo[i][j] == input)
+				{
+					bingo[i][j] = 0;
+				}
+			}
+		}
+		//직선 빙고 처리
+		for (int i = 0; i < 5; i++)
+		{
+			if (bingo[i][0] == 0 && bingo[i][1] == 0 && bingo[i][2] == 0 && bingo[i][3] == 0 && bingo[i][4] == 0)
+			{
+				if (check[i] != true)
+				{
+					check[i] = true;
+					clear += 1;
+				}
+			}
+			if (bingo[0][i] == 0 && bingo[1][i] == 0 && bingo[2][i] == 0 && bingo[3][i] == 0 && bingo[4][i] == 0)
+			{
+				if (check[i + 5] != true)
+				{
+					check[i + 5] = true;
+					clear += 1;
+				}
+			}
+		}
+		//대각선 빙고 처리
+		if (bingo[0][0] == 0 && bingo[1][1] == 0 && bingo[2][2] == 0 && bingo[3][3] == 0 && bingo[4][4] == 0)
+		{
+			if (check[10] != true)
+			{
+				check[10] = true;
+				clear += 1;
+			}
+		}
+		if (bingo[0][4] == 0 && bingo[1][3] == 0 && bingo[2][2] == 0 && bingo[3][1] == 0 && bingo[4][0] == 0)
+		{
+			if (check[11] != true)
+			{
+				check[11] = true;
+				clear += 1;
+			}
+		}
 	}
-
-	std::cout << "\nPlayer2 : ";
-	for (int c = 7; c < 14; c++)
-	{
-		std::cout << card[player[c]] << " ";
-	}
+	return 0;
 }
