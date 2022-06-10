3c67c30d79789c764a6608edee71cef7653a20b1
LDH <ldh1923@naver.com>
2022-06-10 10:34:33 +0900

    2번 문제

--- 220610_Test/220610_Test/220610_Test.cpp
+++ 220610_Test/220610_Test/220610_Test.cpp
@@ -1,32 +1,11 @@
 ﻿#include <iostream>
-#include "220610_Test.h"
 
-void swap1(int* a, int* b)
-{
-    int c = *a;
-    *a = *b;
-    *b = c;
-}
-void swap2(int& a, int& b)
-{
-    int c = a;
-    a = b;
-    b = c;
-}
 int main(void)
 {
-    int a = 10;
-    int b = 20;
-    
-    swap1(&a, &b);
-
-    printf("%d\n", a);
-    printf("%d\n", b);
-    
-    swap2(a, b);
-
-    printf("%d\n",a);
-    printf("%d\n",b);
-
-    return 0;
+    int a = 3;
+    while (a < 1000)
+    {
+        std::cout << a << std::endl;
+        a *= 3;
+    }
 }
