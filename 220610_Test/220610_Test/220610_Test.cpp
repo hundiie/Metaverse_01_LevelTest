19002f6bdcea3dbea5bdc7c5d6967594e9c50d4d
LDH <ldh1923@naver.com>
2022-06-10 14:33:42 +0900

    6번 문제

--- 220610_Test/220610_Test/220610_Test.cpp
+++ 220610_Test/220610_Test/220610_Test.cpp
@@ -1,16 +1,44 @@
 ﻿#include <iostream>
+#include <string>
 
 int main(void)
 {
-	int max;
-	int create;
+	std::string card[53];
+	std::string mark[4] = { "♠","♥","◆","♣" };
+	std::string num[13] = { "A ","2 ","3 ","4 ","5 ","6 ","7 ","8 ","9 ","10","J ","Q ","K " };
+
+	card[52] = "JOKER";
+
+	for (int k = 0; k < 53; k++)
+	{
+		card[k] = mark[k % 4] + num[k % 13];
+	}
 
 	srand(time(NULL));
+	int player[14] = {};
 
-	std::cin >> max >> create;
-	
-	for (int i = 0; i < create; i++)
+	for (int i = 0; i < 14; i++)
 	{
-		std::cout << rand() %max + 1 << std::endl;
+		player[i] = rand() % 53;
+		for (int j = 0; j < i; j++)
+		{
+			if (player[j] == player[i])
+			{
+				i--;
+				break;
+			}
+		}
+	}
+
+	std::cout << "Player1 : ";
+	for (int c = 0; c < 7; c++)
+	{
+		std::cout << card[player[c]] << " ";
+	}
+
+	std::cout << "\nPlayer2 : ";
+	for (int c = 7; c < 14; c++)
+	{
+		std::cout << card[player[c]] << " ";
 	}
 }
