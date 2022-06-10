972ed5a4374ce4a7cfdbb747d9f5832543625a6e
LDH <ldh1923@naver.com>
2022-06-10 10:30:31 +0900

    1번 문제

---
+++ 220610_Test/220610_Test/220610_Test.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include "220610_Test.h"
+
+void swap1(int* a, int* b)
+{
+    int c = *a;
+    *a = *b;
+    *b = c;
+}
+void swap2(int& a, int& b)
+{
+    int c = a;
+    a = b;
+    b = c;
+}
+int main(void)
+{
+    int a = 10;
+    int b = 20;
+    
+    swap1(&a, &b);
+
+    printf("%d\n", a);
+    printf("%d\n", b);
+    
+    swap2(a, b);
+
+    printf("%d\n",a);
+    printf("%d\n",b);
+
+    return 0;
+}
